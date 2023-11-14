4a653a83c36ceb1befd15019ea930d7b5df47edf
JoelKanyi <joelkanyi98@gmail.com>
2023-11-14 18:26:19 +0300

    make UserRepositoryImpl Transactional

--- src/main/kotlin/com/joelkanyi/mealtime/api/mealtimeapi/user/data/UserRepositoryImpl.kt
+++ src/main/kotlin/com/joelkanyi/mealtime/api/mealtimeapi/user/data/UserRepositoryImpl.kt
@@ -6,8 +6,10 @@ import com.joelkanyi.mealtime.api.mealtimeapi.user.model.UserData
 import com.joelkanyi.mealtime.api.mealtimeapi.user.model.UserData.Companion.toUserData
 import org.jetbrains.exposed.sql.select
 import org.springframework.stereotype.Repository
+import org.springframework.transaction.annotation.Transactional
 
 @Repository("user-repository")
+@Transactional
 class UserRepositoryImpl : UserRepository {
     private val userTable = UserTable
 
