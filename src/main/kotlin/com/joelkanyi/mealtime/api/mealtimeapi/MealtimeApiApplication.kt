27009705c2072d7dc1fc50ead1c6a31744671e2a
JoelKanyi <joelkanyi98@gmail.com>
2023-11-14 16:20:29 +0300

    exclude = [JdbcTemplateAutoConfiguration::class]

--- src/main/kotlin/com/joelkanyi/mealtime/api/mealtimeapi/MealtimeApiApplication.kt
+++ src/main/kotlin/com/joelkanyi/mealtime/api/mealtimeapi/MealtimeApiApplication.kt
@@ -1,11 +1,12 @@
 package com.joelkanyi.mealtime.api.mealtimeapi
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.jdbc.JdbcTemplateAutoConfiguration
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
+@SpringBootApplication(exclude = [JdbcTemplateAutoConfiguration::class])
 class MealtimeApiApplication
 
 fun main(args: Array<String>) {
-	runApplication<MealtimeApiApplication>(*args)
+    runApplication<MealtimeApiApplication>(*args)
 }
