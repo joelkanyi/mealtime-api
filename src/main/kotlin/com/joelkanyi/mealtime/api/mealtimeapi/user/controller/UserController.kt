562bfc6026b160615a07dd547a9b97d6c83785d2
JoelKanyi <joelkanyi98@gmail.com>
2023-11-14 18:23:06 +0300

    remove path variable

--- src/main/kotlin/com/joelkanyi/mealtime/api/mealtimeapi/user/controller/UserController.kt
+++ src/main/kotlin/com/joelkanyi/mealtime/api/mealtimeapi/user/controller/UserController.kt
@@ -3,7 +3,6 @@ package com.joelkanyi.mealtime.api.mealtimeapi.user.controller
 import com.joelkanyi.mealtime.api.mealtimeapi.user.model.UserData
 import com.joelkanyi.mealtime.api.mealtimeapi.user.service.UserService
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -13,7 +12,7 @@ class UserController(
     private val userService: UserService
 ) {
     @GetMapping
-    fun getUser(@PathVariable userId: String): UserData {
+    fun getUser(userId: String): UserData {
         return userService.getUser(userId)
     }
 }
