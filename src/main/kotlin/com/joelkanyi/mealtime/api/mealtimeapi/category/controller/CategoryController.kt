f382de1a171c0a37bf6b1cb7b54e17c094e1e763
JoelKanyi <joelkanyi98@gmail.com>
2023-11-14 18:43:33 +0300

    add response status

--- src/main/kotlin/com/joelkanyi/mealtime/api/mealtimeapi/category/controller/CategoryController.kt
+++ src/main/kotlin/com/joelkanyi/mealtime/api/mealtimeapi/category/controller/CategoryController.kt
@@ -3,6 +3,7 @@ package com.joelkanyi.mealtime.api.mealtimeapi.category.controller
 import com.joelkanyi.mealtime.api.mealtimeapi.category.data.dto.CreateCategoryDto
 import com.joelkanyi.mealtime.api.mealtimeapi.category.model.Category
 import com.joelkanyi.mealtime.api.mealtimeapi.category.service.CategoryService
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
 
 @RestController
@@ -11,9 +12,11 @@ class CategoryController(
     private val categoryService: CategoryService
 ) {
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     fun addCategory(
-        @RequestBody category: CreateCategoryDto) {
-        categoryService.addCategory(category)
+        @RequestBody category: CreateCategoryDto
+    ): String {
+        return categoryService.addCategory(category)
     }
 
     @GetMapping
@@ -27,6 +30,7 @@ class CategoryController(
     }
 
     @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     fun deleteCategory(@PathVariable id: Int) {
         categoryService.deleteCategory(id)
     }
