f17836e56cf7b32153b949a8db00031ea0931d34
JoelKanyi <joelkanyi98@gmail.com>
2023-11-25 16:07:48 +0300

    add some logs on auth repo

--- src/main/kotlin/com/joelkanyi/mealtime/api/mealtimeapi/auth/data/repository/AuthRepositoryImpl.kt
+++ src/main/kotlin/com/joelkanyi/mealtime/api/mealtimeapi/auth/data/repository/AuthRepositoryImpl.kt
@@ -3,19 +3,19 @@ package com.joelkanyi.mealtime.api.mealtimeapi.auth.data.repository
 import com.joelkanyi.mealtime.api.mealtimeapi.auth.data.database.UserTable
 import com.joelkanyi.mealtime.api.mealtimeapi.auth.data.database.rowToUser
 import com.joelkanyi.mealtime.api.mealtimeapi.auth.model.*
-import com.joelkanyi.mealtime.api.mealtimeapi.user.model.UserData.Companion.toUserData
 import com.joelkanyi.mealtime.api.mealtimeapi.config.JwtService
-import com.joelkanyi.mealtime.api.mealtimeapi.user.model.UserData
+import com.joelkanyi.mealtime.api.mealtimeapi.user.model.UserData.Companion.toUserData
 import org.jetbrains.exposed.sql.insert
 import org.jetbrains.exposed.sql.select
 import org.jetbrains.exposed.sql.selectAll
+import org.slf4j.LoggerFactory
 import org.springframework.security.authentication.AuthenticationManager
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Repository
 import org.springframework.transaction.annotation.Transactional
 import java.util.*
-import kotlin.NoSuchElementException
+import java.util.logging.Logger
 
 @Repository("auth_repository")
 @Transactional
@@ -25,6 +25,8 @@ class AuthRepositoryImpl(
     private val authenticationManager: AuthenticationManager,
 ) : AuthRepository {
     private val userTable = UserTable
+    val logger: org.slf4j.Logger = LoggerFactory.getLogger(AuthRepositoryImpl::class.java)
+
     override fun findByEmail(email: String): User? {
         return userTable.select { userTable.email eq email }.map { rowToUser(it) }.firstOrNull()
             ?: throw NoSuchElementException("Could not find a user with email $email")
@@ -66,9 +68,11 @@ class AuthRepositoryImpl(
                 loginRequest.password
             )
         )
+        logger.info("Login request for user ${loginRequest.email}")
         val user = findByEmail(loginRequest.email)
             ?: throw NoSuchElementException("Could not find a user with email ${loginRequest.email}")
         val jwtToken = jwtService.generateToken(user)
+        logger.info("Login request for user ${loginRequest.email} found user $user")
         val userFromDb = findByEmail(loginRequest.email) ?: throw NoSuchElementException("Could not find a user with email ${loginRequest.email}")
         return AuthenticationResponse(
             jwtToken,
@@ -78,8 +82,10 @@ class AuthRepositoryImpl(
 
     override fun refreshToken(token: String): AuthenticationResponse {
         val userName = jwtService.extractUserName(token)
+        logger.info("Refreshing token for user $userName")
         val userDetails = findByEmail(userName)
             ?: throw NoSuchElementException("Could not find a user with email $userName")
+        logger.info("Refresh token Found user $userDetails")
         return login(LoginRequest(userDetails.email, userDetails.password))
     }
 }
